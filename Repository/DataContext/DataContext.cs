2f375d1db60ffb2ad419038c1d567047e28a9966
Vitor Castro <vitor.castro@subscriptionfactory.com>
2025-02-16 21:13:55 +0000

    update

---
+++ Repository/DataContext/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using ZIP2GO.Service.Models;
+
+namespace Repository.DataContext
+{
+    public class DataContext : DbContext
+    {
+        private readonly string _connectionString;
+
+        public DataContext(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+        //other models
+        //with YourName if you have for example Book Model then it could be something like this:
+        public DbSet<Subscription> Subscriptions { get; set; }
+    }
+}
