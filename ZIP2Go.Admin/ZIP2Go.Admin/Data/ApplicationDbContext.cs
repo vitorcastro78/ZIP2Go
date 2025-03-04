578f42ac850be0b60a2a62432cf8c37b90489438
Vitor Castro <vitorcastro78@gmail.com>
2025-03-04 15:09:32 +0000

    delete unused projects

--- ZIP2Go.Admin/ZIP2Go.Admin/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace ZIP2Go.Admin.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
-}
